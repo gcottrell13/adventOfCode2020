8d364d232821eee8813c76b3984157d91aa925b0
Gage <inquisitorpinkie@gmail.com>
2020-12-07 21:44:59 -0700

    init

---
+++ adventOfCode/Program.cs
@@ -0,0 +1,11 @@
+﻿using adventOfCode.days;
+
+namespace adventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ adventOfCode/code.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace adventOfCode
+{
+    class Code
+    {
+        protected string read(string file)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = assembly.GetManifestResourceNames().Single(str => str.EndsWith($"{file}.txt"));
+
+            using Stream stream = assembly.GetManifestResourceStream(resourceName);
+            using StreamReader reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+    }
+}
